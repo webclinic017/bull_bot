60060a2a9b8d9c1b297b9911502f16680e15e019
Connor Malley <connormalley98@gmail.com>
2020-08-25 22:08:16 -0400

    Added BullGraph class, and cleaned code

--- bull_bot.py
+++ bull_bot.py
@@ -15,95 +15,134 @@ import dash_html_components as html
 import robin_stocks as rh
 from dash.dependencies import Input, Output
 
+
 start_date = datetime(2016, 1, 1)
 end_date   = datetime(2020, 7, 27)
 
 colors = {
-    "text" : "#7FDBFF",
+    "text" : "#FFFFFF",
     "background" : "#63995c"
 }
+
+class BullGraph(object):
+    # TODO: Change to use relative dates from current date
+    def __init__(self, ticker="NVDA", start_date=start_date, end_date=end_date):
+        self.start_date = start_date
+        self.end_date   = end_date
+        self.ticker     = ticker
+        self.createFig()
+
+    def createFig(self): 
+        """
+            createFig(ticker)
+            =================
+                Description: Creates historical candlestick graph for the input stock ticker.
+
+                Inputs:
+                    - ticker (str): string representing a stock ticker (e.g. NVDA, AMD, FCEL, etc...)
+
+                Output:
+                    - None
+
+                Return(s):
+                    - fig (go.Figure): figure object
+        """
+        try:
+            self.historical = data.DataReader(self.ticker, "yahoo", self.start_date, self.end_date )
+        except IOError:
+            self.historical = data.DataReader("NVDA", "yahoo", self.start_date, self.end_date )
+
+        print(self.historical)        
+        self.historical.reset_index(inplace=True, drop=False)
+
+        # Creating graph
+        self.fig = go.Figure(
+            data=[go.Candlestick(x=self.historical['Date'],
+            open=self.historical['Open'],
+            high=self.historical['High'],
+            low=self.historical['Low'],
+            close=self.historical['Close'])])
+
+        return self.fig
+
+    def styleFig(self):
+        if not self.fig:
+            # Creating graph
+            self.fig = go.Figure(
+                data=[go.Candlestick(x=historical['Date'],
+                open=self.historical['Open'],
+                high=self.historical['High'],
+                low=self.historical['Low'],
+                close=self.historical['Close'])])
+
+
+        # Styling the graph
+        self.fig.layout = dict({
+            "title_x" : 0.5,
+            "xaxis_title" : "<b>Date</b>",
+            "yaxis_title" : "<b>Price</b>",
+            "paper_bgcolor": colors["background"],
+            "font" : dict(
+                color=colors["text"],
+                size=18
+            )
+        })
+
+
+        self.fig.update_xaxes(
+            rangebreaks=[dict(bounds=["sat", "mon"])],
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all")
+                ])
+            )
+        )
+
+        return self.fig, self.historical
 
 
 
 # ROBIN HOOD
-
 # Grabs password
-passfile = open("rh_pass.txt", "r")
-password = passfile.read()
-passfile.close()
-
-rh.authentication.login(username="malleyconnor@knights.ucf.edu", password=password)
-
-securities = rh.profiles.load_security_profile()
-portfolio  = rh.profiles.load_portfolio_profile()
-holdings   = rh.account.build_holdings()
-
-security_list = ""
-for security in holdings:
-    this_security = f"%s -- %s\n" % (holdings[security]["name"], holdings[security]["price"])
-    security_list = f"%s%s" % (security_list, this_security)
-
-security_list = f"<b>%s</b>" % security_list
-
-
-
-
-def createFig(ticker="NVDA"): 
+def rh_login(username=""):
     """
-        createFig(ticker)
-        =================
-            Description: Creates historical candlestick graph for the input stock ticker.
+    rh_login(username)
+    ==================
+        Description:    Logs into robinhood with the specified username and password, 
+                        and grabs data of current positions
 
-            Inputs:
-                - ticker (str): string representing a stock ticker (e.g. NVDA, AMD, FCEL, etc...)
+        Input(s):
+            - username (str): username/email of robinhood account
 
-            Output:
-                - None
+        Output(s):
+            - None
 
-            Return(s):
-                - fig (go.Figure): figure object
+        Return(s):
+            - security_list (str): String displaying current positions
     """
-    print(f"%s : %s" % (ticker, str(type(ticker))))
-    df = data.DataReader(ticker, "yahoo", start_date, end_date )
-    df.reset_index(inplace=True, drop=False)
+    passfile = open("rh_pass.txt", "r")
+    password = passfile.read()
+    passfile.close()
 
-    # Creating graph
-    fig = go.Figure(
-        data=[go.Candlestick(x=df['Date'],
-        open=df['Open'],
-        high=df['High'],
-        low=df['Low'],
-        close=df['Close'])])
+    rh.authentication.login(username="malleyconnor@knights.ucf.edu", password=password)
 
-    ############################
-    # Styling the graph
-    fig.layout = dict({
-        "title_x" : 0.5,
-        "xaxis_title" : "<b>Date</b>",
-        "yaxis_title" : "<b>Price</b>",
-        "paper_bgcolor": colors["background"],
-        "font" : dict(
-            color="#7f7f7f",
-            size=18
-        )
-    })
+    securities = rh.profiles.load_security_profile()
+    portfolio  = rh.profiles.load_portfolio_profile()
+    holdings   = rh.account.build_holdings()
 
+    security_list = ""
+    for security in holdings:
+        this_security = f"%s -- %s\n" % (holdings[security]["name"], holdings[security]["price"])
+        security_list = f"%s%s" % (security_list, this_security)
 
-    fig.update_xaxes(
-        rangebreaks=[dict(bounds=["sat", "mon"])],
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    )
+    security_list = f"<b>%s</b>" % security_list
 
-    return fig, df
+    return security_list
 
 
 
@@ -111,18 +150,22 @@ app = dash.Dash(__name__)
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
+# Initializing graph
+start_date = datetime(2016, 1, 1)
+end_date   = datetime(2020, 7, 27)
+bg = BullGraph(start_date=start_date, end_date=end_date)
+
+security_list = rh_login("malleyconnor@knights.ucf.edu")
+
 # Creating the Ticker input
 ticker_bar = dcc.Input(id="ticker_in", type="text", placeholder="NVDA", debounce=True)
 
-
-# Initial Configuration
-fig, df = createFig("NVDA")
 app.layout = html.Div(
     style={
         "background-color" : "#63995c"
     },
     children=[
-        dcc.Graph(id="main_graph", figure=fig),
+        dcc.Graph(id="main_graph", figure=bg.fig),
         html.Div(
             children=security_list, 
             style={
@@ -137,12 +180,16 @@ app.layout = html.Div(
 # Rendering the ticker input for the graph
 @app.callback(Output("main_graph", "figure"),
 [Input("ticker_in", "value")])
-def ticker_render(vals="NVDA"):
-    if (vals == None):
-        fig, df = createFig("NVDA")
+def ticker_render(ticker="NVDA"):
+    if (ticker == None):
+        bg.ticker = "NVDA"
+        fig = bg.createFig()
+        fig, historical = bg.styleFig()
         return fig
 
-    fig, df = createFig(vals)
+    bg.ticker = ticker
+    fig = bg.createFig()
+    fig, historical = bg.styleFig()
     return fig
 
 
